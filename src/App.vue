f7a4a731442c53f662c0106a279763249674aba9
chenmincong <chenmincong@aegis-data.cn>
2022-09-05 11:30:59 +0800

    过度和动画

--- src/App.vue
+++ src/App.vue
@@ -1,6 +1,5 @@
 <template>
   <div id="app">
-    <h1>{{ msg }}</h1>
     <Test />
   </div>
 </template>
@@ -21,15 +20,3 @@ export default {
   methods: {}, //
 };
 </script>
-
-<style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-  background-color: rgb(129, 201, 248);
-}
-</style>
--- src/components/TestChange.vue
+++ src/components/TestChange.vue
@@ -1,25 +1,39 @@
 <template>
   <div>
-    <button @click="change">显示/隐藏</button>
-    <h3 class="content">你好啊</h3>
+    <button @click="isShow = !isShow">显示/隐藏</button>
+    <transition>
+      <h1 v-show="isShow">你好啊</h1>
+    </transition>
   </div>
 </template>
 
 <script>
 export default {
   data() {
-    return {};
-  },
-  methods: {
-    change() {
-      
-    },
+    return {
+      isShow: false,
+    };
   },
 };
 </script>
 
-<style lang="scss">
-.content {
+<style lang="scss" scoped>
+h1 {
   background-color: orange;
+}
+.v-enter-active {
+  animation: hello 1s reverse;
+}
+
+.v-leave-active {
+  animation: hello 1s;
+}
+@keyframes hello {
+  form {
+    transform: translateX(0);
+  }
+  to {
+    transform: translateX(-100%);
+  }
 }
 </style>
